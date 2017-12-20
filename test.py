91e6d5462f327206138424194ed87e8ee894d60a
Charles Durham <cpdurham@gmail.com>
2017-12-20 18:09:34 -0500

    Added gradient differences

--- test.py
+++ test.py
@@ -3,45 +3,58 @@ import torch
 from HadamardProduct import HadamardProduct
 
 class Network(torch.nn.Module):
-	def __init__(self):
-		super(Network, self).__init__()
-	# end
+        def __init__(self):
+                super(Network, self).__init__()
 
-	def forward(self, input1, input2):
-		return HadamardProduct()(input1, input2)
-	# end
-# end
+        def forward(self, input1, input2):
+                return HadamardProduct()(input1, input2).mean()
 
 net = Network().cuda()
 for i in range(10):
-	input1 = torch.rand(64, 3, 128, 128).cuda()
-	input2 = torch.rand(64, 3, 128, 128).cuda()
+        input1 = torch.rand(64, 3, 128, 128).cuda()
+        input2 = torch.rand(64, 3, 128, 128).cuda()
 
-	input1 = torch.autograd.Variable(input1, requires_grad=True)
-	input2 = torch.autograd.Variable(input2, requires_grad=True)
+        input1o = torch.autograd.Variable(input1, requires_grad=True)
+        input2o = torch.autograd.Variable(input2, requires_grad=True)
 
-	output = net(input1, input2)
-	expected = torch.mul(input1, input2)
+        input1e = torch.autograd.Variable(input1, requires_grad=True)
+        input2e = torch.autograd.Variable(input2, requires_grad=True)
 
-	print(torch.sum(output.data - expected.data), '<-- should be 0.0')
+        output = net(input1o, input2o)
+        output.backward()
 
-	output.backward(output.data)
-# end
+        expected = torch.mul(input1e, input2e).mean()
+        expected.backward()
+
+        print('Output: \t', (output.data - expected.data).abs().sum(), '<-- should be 0.0')
+
+        print('Gradient1: \t', (input1o.grad.data - input1e.grad.data).abs().sum(), '<-- should be 0.0')
+        print('Gradient2: \t', (input2o.grad.data - input2e.grad.data).abs().sum(), '<-- should be 0.0')
+
+        print()
 
 print('switching to DataParallel mode')
 
 net = torch.nn.DataParallel(Network()).cuda()
 for i in range(10):
-	input1 = torch.rand(64, 3, 128, 128).cuda()
-	input2 = torch.rand(64, 3, 128, 128).cuda()
+        input1 = torch.rand(64, 3, 128, 128).cuda()
+        input2 = torch.rand(64, 3, 128, 128).cuda()
 
-	input1 = torch.autograd.Variable(input1, requires_grad=True)
-	input2 = torch.autograd.Variable(input2, requires_grad=True)
+        input1o = torch.autograd.Variable(input1, requires_grad=True)
+        input2o = torch.autograd.Variable(input2, requires_grad=True)
 
-	output = net(input1, input2)
-	expected = torch.mul(input1, input2)
+        input1e = torch.autograd.Variable(input1, requires_grad=True)
+        input2e = torch.autograd.Variable(input2, requires_grad=True)
 
-	print(torch.sum(output.data - expected.data), '<-- should be 0.0')
+        output = net(input1o, input2o)
+        output.backward()
 
-	output.backward(output.data)
-# end
+        expected = torch.mul(input1e, input2e).mean()
+        expected.backward()
+
+        print('Output: \t', (output.data - expected.data).abs().sum(), '<-- should be 0.0')
+
+        print('Gradient1: \t', (input1o.grad.data - input1e.grad.data).abs().sum(), '<-- should be 0.0')
+        print('Gradient2: \t', (input2o.grad.data - input2e.grad.data).abs().sum(), '<-- should be 0.0')
+
+        print()
