f312b4976e19f45a43d3e77a4c980ca799d4cfb0
sniklaus <simon.niklaus@outlook.com>
2017-12-22 14:59:22 -0800

    no message

--- test.py
+++ test.py
@@ -13,9 +13,9 @@ class Network(torch.nn.Module):
 # end
 
 net = Network().cuda()
-for i in range(10):
-	input1 = torch.rand(64, 3, 128, 128).cuda()
-	input2 = torch.rand(64, 3, 128, 128).cuda()
+for i in range(3):
+	input1 = torch.rand(2, 3, 8, 8).cuda()
+	input2 = torch.rand(2, 3, 8, 8).cuda()
 
 	input1 = torch.autograd.Variable(input1, requires_grad=True)
 	input2 = torch.autograd.Variable(input2, requires_grad=True)
@@ -24,16 +24,15 @@ for i in range(10):
 	expected = torch.mul(input1, input2)
 
 	print(torch.sum(output.data - expected.data), '<-- should be 0.0')
-
-	output.backward(output.data)
+	print(torch.autograd.gradcheck(net, tuple([ input1, input2 ]), 0.001), '<-- should be true')
 # end
 
 print('switching to DataParallel mode')
 
 net = torch.nn.DataParallel(Network()).cuda()
-for i in range(10):
-	input1 = torch.rand(64, 3, 128, 128).cuda()
-	input2 = torch.rand(64, 3, 128, 128).cuda()
+for i in range(3):
+	input1 = torch.rand(2, 3, 8, 8).cuda()
+	input2 = torch.rand(2, 3, 8, 8).cuda()
 
 	input1 = torch.autograd.Variable(input1, requires_grad=True)
 	input2 = torch.autograd.Variable(input2, requires_grad=True)
@@ -42,6 +41,5 @@ for i in range(10):
 	expected = torch.mul(input1, input2)
 
 	print(torch.sum(output.data - expected.data), '<-- should be 0.0')
-
-	output.backward(output.data)
+	print(torch.autograd.gradcheck(net, tuple([ input1, input2 ]), 0.001), '<-- should be true')
 # end
