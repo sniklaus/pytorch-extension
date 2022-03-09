bd54e2baf183fd55dba33a2e16987e0737781abc
Simon Niklaus <simon.niklaus@outlook.com>
2022-03-09 09:25:43 -0800

    no message

--- run.py
+++ run.py
@@ -7,37 +7,37 @@ import hadamard # the custom layer
 ##########################################################
 
 class Network(torch.nn.Module):
-	def __init__(self):
-		super().__init__()
-	# end
+    def __init__(self):
+        super().__init__()
+    # end
 
-	def forward(self, tenOne, tenTwo):
-		return hadamard.hadamard_func.apply(tenOne, tenTwo)
-	# end
+    def forward(self, tenOne, tenTwo):
+        return hadamard.hadamard_func.apply(tenOne, tenTwo)
+    # end
 # end
 
 netNetwork = Network().cuda()
 for intIter in range(3):
-	tenOne = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
-	tenTwo = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
+    tenOne = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
+    tenTwo = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
 
-	tenOut = netNetwork(tenOne, tenTwo)
-	tenExpected = torch.mul(tenOne, tenTwo)
+    tenOut = netNetwork(tenOne, tenTwo)
+    tenExpected = torch.mul(tenOne, tenTwo)
 
-	print((tenOut.data - tenExpected.data).abs().sum(), '<-- should be 0.0')
-	print(torch.autograd.gradcheck(func=netNetwork, inputs=tuple([tenOne, tenTwo]), eps=0.001), '<-- should be true')
+    print((tenOut.data - tenExpected.data).abs().sum(), '<-- should be 0.0')
+    print(torch.autograd.gradcheck(func=netNetwork, inputs=tuple([tenOne, tenTwo]), eps=0.001), '<-- should be true')
 # end
 
 print('switching to DataParallel mode')
 
 netNetwork = torch.nn.DataParallel(Network()).cuda()
 for intIter in range(3):
-	tenOne = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
-	tenTwo = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
+    tenOne = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
+    tenTwo = torch.rand(2, 3, 8, 8).cuda().requires_grad_()
 
-	tenOut = netNetwork(tenOne, tenTwo)
-	tenExpected = torch.mul(tenOne, tenTwo)
+    tenOut = netNetwork(tenOne, tenTwo)
+    tenExpected = torch.mul(tenOne, tenTwo)
 
-	print((tenOut.data - tenExpected.data).abs().sum(), '<-- should be 0.0')
-	print(torch.autograd.gradcheck(func=netNetwork, inputs=tuple([tenOne, tenTwo]), eps=0.001), '<-- should be true')
+    print((tenOut.data - tenExpected.data).abs().sum(), '<-- should be 0.0')
+    print(torch.autograd.gradcheck(func=netNetwork, inputs=tuple([tenOne, tenTwo]), eps=0.001), '<-- should be true')
 # end
